7914957531aaeec89043235fc1b9b23118bfda6f
thiago <thiagoaugustobewiahn@gmail.com>
2023-04-11 09:28:17 -0300

    Improvement on partial pivot with index vector

--- eliminacaoGauss.py
+++ eliminacaoGauss.py
@@ -1,3 +1,5 @@
+import random
+
 def gauss_elimination(A, b):
     """
     Solves the system of linear equations Ax = b using Gaussian elimination.
@@ -62,38 +64,48 @@ def no_pivot(A, b):
 def partial_pivot(A, b):
     n = len(A)
     
+    # index vector for changing the order of the rows
+    o = [i for i in range(n)]
+    
     for k in range(n - 1):
         # Find the row with the largest absolute value in column i
         max_row = k
         for j in range(k+1, n):
-            if abs(A[j][k]) > abs(A[max_row][k]):
+            if abs(A[o[j]][k]) > abs(A[o[max_row]][k]):
                 max_row = j
-        # Swap the max row with the current row i
-        A[k], A[max_row] = A[max_row], A[k]
-        b[k], b[max_row] = b[max_row], b[k]
+        
+        o[k], o[max_row] = o[max_row], o[k]
+        
         for i in range(k + 1, n):
-            factor = A[i][k] / A[k][k]
-            b[i] -= b[k] * factor
+            factor = A[o[i]][k] / A[o[k]][k]
+            b[o[i]] -= b[o[k]] * factor
             for j in range(k, n):
-                A[i][j] -= A[k][j] * factor
+                A[o[i]][j] -= A[o[k]][j] * factor
                 
     # Backward substitution
     x = [0] * n
     for i in range(n-1, -1, -1):
-        x[i] = b[i] / A[i][i]
+        x[i] = b[o[i]] / A[o[i]][i]
         for j in range(i+1, n):
-            x[i] -= A[i][j] * x[j] / A[i][i]
-            
+            x[i] -= A[o[i]][j] * x[j] / A[o[i]][i]
+    
+    print(o)
     return x
 
 def main():
-    A = [
+    ''' A = [
         [1, -1, 1],
         [2, 3, -1],
         [-3, 1, 1]
     ]
     
-    b = [1, 4, -1]
+    b = [1, 4, -1] '''
+    
+    size = 100
+    
+    A = [[random.randint(1, 10) for i in range(size)] for j in range(size)]
+    
+    b = [random.randint(1, 10) for i in range(size)]
     
     print(partial_pivot(A, b))
 
