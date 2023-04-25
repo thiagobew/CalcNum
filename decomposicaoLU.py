b0125bd4cef37403590c0a857b566c0e5e35e12e
thiago <thiagoaugustobewiahn@gmail.com>
2023-04-25 14:47:44 -0300

    Fixed LU conflicts

--- decomposicaoLU.py
+++ decomposicaoLU.py
@@ -1,9 +1,9 @@
 def doolittle(A, b):
     n = len(A)
-    
+
     L = [[None] * n for i in range(n)]
     U = [[None] * n for i in range(n)]
-    
+
     for i in range(n):
         for j in range(n):
             if i == j:
@@ -12,26 +12,25 @@ def doolittle(A, b):
                 U[i][j] = 0
             else:
                 L[i][j] = 0
-    
-    
+
     for i in range(n):
         L[i][0] = A[i][0]
         for j in range(n + 1):
             U[0][j] = A[0][j] / L[0][0]
-    
+
     for k in range(1, n):
         for i in range(k, n):
             soma = 0
             for p in range(k):
                 soma += L[i][p] * U[p][k]
             U[i][k] = A[i][k] - soma
-        
+
         for j in range(k, n):
             soma = 0
             for p in range(k):
                 soma += L[k][p] * U[p][j]
             L[k][j] = (A[k][j] - soma) / U[k][k]
-            
+
     return L, U
     
 def crout(A: list, b):
