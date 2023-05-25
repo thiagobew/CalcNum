ef4f224ee0fe90e50de4706901b2a2105796bd53
thiago <thiagoaugustobewiahn@gmail.com>
2023-05-25 09:32:14 -0300

    quadratura gaussiana

--- integral.py
+++ integral.py
@@ -18,7 +18,7 @@ def trapezio(f: callable, a: float, b: float, n: int):
 
 def simpson(f: callable, a: float, b: float, n: int):
     n = int(n)
-    
+
     h = (b-a)/n
     x = np.linspace(a, b, n+1)
     y = f(x)
@@ -30,7 +30,7 @@ def simpson(f: callable, a: float, b: float, n: int):
 
 def compute_integral(f: callable, a: float, b: float, method: str = "trapezio", start_n: int = 20):
     n = int(start_n)
-    
+
     method_func = None
     if method == "trapezio":
         method_func = trapezio
@@ -38,16 +38,37 @@ def compute_integral(f: callable, a: float, b: float, method: str = "trapezio", 
         method_func = simpson
     else:
         raise ValueError("Invalid method")
-    
-    y_int = method_func(f, a, b, n)
-    return y_int, n
-    
 
+    y_bef = 0
+    while True:
+        y = method_func(f, a, b, n)
+        if np.abs(y - y_bef) < 1e-16:
+            break
+        y_bef = y
+        n *= 2
+        
+    return y, n
+
+
+def load_points(n: int = 64):
+    return np.polynomial.legendre.leggauss(n)
+
+
+def quadratura_gaussiana(f: callable, a: float, b: float, points: np.ndarray, weights: np.ndarray):
+    x = (b-a)/2 * points + (b+a)/2
+    y = f(x)
+    y_int = (b-a)/2 * np.sum(weights * y)
+    return y_int
 
 
 def main():
-    print(compute_integral(f, 0, 1, "trapezio"))
-    print(compute_integral(f, 0, 1, "simpson"))
+    a = 2
+    b = 6
+    #print(compute_integral(f, 0, 1, "trapezio"))
+    print(compute_integral(f, a, b, "simpson"))
+    
+    points, weights = load_points()
+    print(quadratura_gaussiana(f, a, b, points, weights))
 
 
 if __name__ == "__main__":
